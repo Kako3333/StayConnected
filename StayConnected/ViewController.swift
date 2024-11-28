aa17d4be12b26cb228812e5ac94205ca397b0e3b
Giorgi Kalaladze <giokakaladze4@gmail.com>
2024-11-28 14:34:49 +0400

    initial commit

---
+++ StayConnected/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StayConnected
+//
+//  Created by Gio Kakaladze on 28.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
