b593e115ec24c862afc81ff1865944b12b92b18a
TinaTinaJ <jishkarianitiko@gmail.com>
2024-11-30 14:02:50 +0400

    Add files via upload

---
+++ StayConnected/MainApp/HomePage/EmptyStateView.swift
@@ -0,0 +1,72 @@
+//
+//  EmptyStateView.swift
+//  StayConnected
+//
+//  Created by Tiko on 29.11.24.
+//
+import UIKit
+
+class EmptyStateView: UIView {
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textAlignment = .center
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.textColor = UIColor(hex: "777E99")
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        addSubview(imageView)
+        addSubview(titleLabel)
+        addSubview(messageLabel)
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 40),
+            imageView.widthAnchor.constraint(equalToConstant: 238),
+            imageView.heightAnchor.constraint(equalToConstant: 230),
+            
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            
+            messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
+            messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20)
+        ])
+    }
+    
+    func configure(image: UIImage, title: String, message: String) {
+        imageView.image = image
+        titleLabel.text = title
+        messageLabel.text = message
+    }
+}
+
